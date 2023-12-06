cef1b6ddea06c5240e2da054bf971898f8abf6e7
Abin <aruvinnwijaya@gmail.com>
2023-12-06 18:53:49 +0700

    first commit

---
+++ web/src/components/Perkerja.js
@@ -0,0 +1,18 @@
+const Perkerja = [
+    {
+        id:"1",
+        Nama:"Alvin Wijaya",
+        Status:"Belum Menikah"
+    }, {
+        id:"2",
+        Nama:"Intana",
+        Status:"Belum Menikah"
+    }, {
+        id:"3",
+        Nama:"Junadi Jondo",
+        Status:"Menikah"
+    }
+
+]
+
+export default Perkerja;
